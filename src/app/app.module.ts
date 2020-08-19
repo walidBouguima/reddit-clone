2d2c6bf4641513ad9eed16f2e7a214a65154ab2d
Walid Bouguima <walid.bouguima@gmail.com>
2020-08-19 10:36:19 +0100

    Add new component to show the data submitted through the form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ArticleComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
