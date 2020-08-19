79a7bd5b9898e8838a039c89d6ec67af2780c0be
Walid Bouguima <walid.bouguima@gmail.com>
2020-08-19 10:59:19 +0100

    Finish working on the child template and logic. Intersting: Using the @HostBinding() is nice because it means we can encapsulate the app-article markup within our component. That is, we don’t have to both use an app-article tag and require a class=row in the markup of the parent view. By using the @HostBinding decorator, we’re able to configure our host element from within the component.

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core'
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row'
+  votes: number
+  title: string
+  link: string
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.title = 'Angular'
+    this.link = 'http://angular.io'
+    this.votes = 10
+  }
+  voteUp() {
+    this.votes += 1
+  }
+  // tslint:disable-next-line: typedef
+  voteDown() {
+    this.votes -= 1
   }
 
+  ngOnInit(): void {}
 }
