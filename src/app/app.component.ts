0c26fd19c38e6c80cd3acc68d40a2ee7c47f52ae
Walid Bouguima <walid.bouguima@gmail.com>
2020-08-19 13:07:38 +0100

    Encapsulate the methods in the model, storing multiple articles in the app component, configuring the article with @Input(), Rendering a list of articles with *ngFor in the app component on the selector of the article component, modify the addArticle function to allow the insertion of new article in the articles array using push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { Article } from './article/article.model'
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,21 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  articles: Article[]
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 123),
+      new Article('React', 'http://react.io', 89),
+      new Article('Ember', 'http://ember.io', 55),
+      new Article('Vanilla', 'http://vanilla.io', 20),
+    ]
+  }
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`)
+    this.articles.push(new Article(title.value, link.value, 0))
+    title.value = ''
+    link.value = ''
     return false
   }
 }
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from '@angular/core'
+import { Component, OnInit, HostBinding, Input } from '@angular/core'
 import { Article } from './article.model'
 @Component({
   selector: 'app-article',
@@ -7,18 +7,16 @@ import { Article } from './article.model'
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row'
-  article: Article
+  @Input() article: Article
 
-  constructor() {
-    this.article = new Article('Rich', 'https://rich.com', 24)
-  }
+  constructor() {}
   voteUp() {
-    this.article.votes += 1
+    this.article.voteUp()
     return false
   }
   // tslint:disable-next-line: typedef
   voteDown() {
-    this.article.votes -= 1
+    this.article.voteDown()
     return false
   }
 
